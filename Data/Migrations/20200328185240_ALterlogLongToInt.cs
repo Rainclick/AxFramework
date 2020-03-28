8cc3d82bb187ab5506131574381ab4883a0c21d4
AmirNorouzpour <amirnorozpour@gmail.com>
2020-03-28 23:34:59 +0430

    -

---
+++ Data/Migrations/20200328185240_ALterlogLongToInt.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class ALterlogLongToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Logs",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Logs",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
