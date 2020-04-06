819867bbaf4d526f9df2169d0189bebdbad45267
AmirNorouzpour <amirnorozpour@gmail.com>
2020-04-06 19:58:42 +0430

    -

---
+++ Entities/Framework/AxChart.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Framework
+{
+    public class AxChart : BaseEntity
+    {
+        public string Title { get; set; }
+        public bool Active { get; set; }
+        public AxChartType Type { get; set; }
+        public long? SystemId { get; set; }
+        [ForeignKey("SystemId")]
+        public Menu System { get; set; }
+    }
+
+    public class PieChart : AxChart
+    {
+
+    }
+
+    public enum AxChartType
+    {
+        List,
+        Pie,
+        Line,
+        Column,
+        Area
+    }
+
+
+}
