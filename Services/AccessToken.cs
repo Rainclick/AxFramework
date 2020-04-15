85061b0ada614fe6808df15d6f91ff6a10f98953
AmirNorouzpour <amirnorozpour@gmail.com>
2020-04-15 14:35:42 +0430

    -

--- Services/AccessToken.cs
+++ Services/AccessToken.cs
@@ -8,13 +8,14 @@ namespace Services
         public string access_token { get; set; }
         public string refresh_token { get; set; }
         public string token_type { get; set; }
-        public int expires_in { get; set; }
+        public long expires_in { get; set; }
 
         public AccessToken(JwtSecurityToken securityToken)
         {
             access_token = new JwtSecurityTokenHandler().WriteToken(securityToken);
             token_type = "Bearer";
-            expires_in = (int)(securityToken.ValidTo - DateTime.UtcNow).TotalSeconds;
+            var datetime = DateTime.Parse("1970-01-01T00:00:00");
+            expires_in = (long)(securityToken.ValidTo - datetime.ToUniversalTime()).TotalSeconds;
         }
     }
 }
