c8bf1ac03bcbb8c0c64877869d887d92935ba712
AmirNorouzpour <amirnorozpour@gmail.com>
2020-04-22 18:28:07 +0430

    -

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,7 +6,6 @@ using Common;
 using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NLog;
--- WebFramework/UserData/ReportExtensions.cs
+++ WebFramework/UserData/ReportExtensions.cs
@@ -15,29 +15,32 @@ namespace WebFramework.UserData
     {
         public static object Execute0<T>(this Report report) where T : BaseEntity
         {
-            var repository = AutoFacSingleton.Instance.Resolve<IBaseRepository<T>>();
             object data;
-            Filter<T> predicate = null;
-            if (report.Filters != null && report.Filters.Any())
+            using (var threadLifetime = AutoFacSingleton.Instance.BeginLifetimeScope())
             {
-                var request = new DataRequest { Filters = new List<AxFilter>() };
-                request.Filters.AddRange(report.Filters);
-                predicate = request.GetFilter<T>();
-            }
+                var repository = threadLifetime.Resolve<IBaseRepository<T>>();
+                Filter<T> predicate = null;
+                if (report.Filters != null && report.Filters.Any())
+                {
+                    var request = new DataRequest { Filters = new List<AxFilter>() };
+                    request.Filters.AddRange(report.Filters);
+                    predicate = request.GetFilter<T>();
+                }
 
-            switch (report.ExecuteType)
-            {
-                case ReportType.All:
-                    data = !string.IsNullOrWhiteSpace(report.Sort) ? repository.GetAll(predicate).OrderBy(report.Sort, report.SortType).Take(report.TakeSize > 0 ? report.TakeSize : int.MaxValue) : repository.GetAll(predicate).Take(report.TakeSize > 0 ? report.TakeSize : int.MaxValue);
-                    break;
-                case ReportType.First:
-                    data = !string.IsNullOrWhiteSpace(report.Sort) ? repository.GetAll(predicate).OrderBy(report.Sort, report.SortType).Take(1) : repository.GetAll(predicate).Take(1);
-                    break;
-                case ReportType.Count:
-                    data = repository.Count(predicate);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException($"report '{report.Title}' executeType is not valid");
+                switch (report.ExecuteType)
+                {
+                    case ReportType.All:
+                        data = !string.IsNullOrWhiteSpace(report.Sort) ? repository.GetAll(predicate).OrderBy(report.Sort, report.SortType).Take(report.TakeSize > 0 ? report.TakeSize : int.MaxValue) : repository.GetAll(predicate).Take(report.TakeSize > 0 ? report.TakeSize : int.MaxValue);
+                        break;
+                    case ReportType.First:
+                        data = !string.IsNullOrWhiteSpace(report.Sort) ? repository.GetAll(predicate).OrderBy(report.Sort, report.SortType).Take(1) : repository.GetAll(predicate).Take(1);
+                        break;
+                    case ReportType.Count:
+                        data = repository.Count(predicate);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"report '{report.Title}' executeType is not valid");
+                }
             }
             return data;
         }
